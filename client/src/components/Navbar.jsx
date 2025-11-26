ef6a25d309c5bb8cfa013d4bd6dc1bb856fa96a3
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2025-11-26 21:06:30 +0530

    modified src/components/Navbar.jsx file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,9 @@
-import { Link } from "react-router-dom";
+import React from 'react'
 
-export default function Navbar({ cartCount }) {
+const Navbar = () => {
   return (
-    <div style={styles.container}>
-      <Link to="/" style={styles.link}>E-Shop</Link>
-      <Link to="/cart" style={styles.link}>Cart ({cartCount})</Link>
-    </div>
-  );
+    <div>Navbar</div>
+  )
 }
 
-const styles = {
-  container: {
-    padding: "15px",
-    background: "#0C3866",
-    color: "#fff",
-    display: "flex",
-    justifyContent: "space-between",
-    fontSize: "20px"
-  },
-  link: {
-    color: "#fff",
-    textDecoration: "none"
-  }
-};
+export default Navbar
