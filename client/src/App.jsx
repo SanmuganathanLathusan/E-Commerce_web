db4aba0c62f542b60ceddace961adaf5eeba1739
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2025-11-26 21:04:15 +0530

    modified src/App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[pvw]'>
+      <Routes>
+
+      </Routes>
+    </div>
   )
 }
 
