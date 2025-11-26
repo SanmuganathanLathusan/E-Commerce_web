e67f86474e60797e708d80ce4211f26344f7fb4d
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2025-11-26 21:07:16 +0530

    modified src/components/ProductCart.jsx file

--- client/src/components/ProductCart.jsx
+++ client/src/components/ProductCart.jsx
@@ -1,39 +1,9 @@
-import { Link } from "react-router-dom";
+import React from 'react'
 
-export default function ProductCard({ item }) {
+const ProductCart = () => {
   return (
-    <div style={styles.card}>
-      <img src={item.image} style={styles.img} alt="" />
-      <h3>{item.name}</h3>
-      <p>${item.price}</p>
-      <Link to={`/product/${item.id}`} style={styles.btn}>
-        View Details
-      </Link>
-    </div>
-  );
+    <div>ProductCart</div>
+  )
 }
 
-const styles = {
-  card: {
-    border: "1px solid #ddd",
-    padding: 20,
-    borderRadius: 10,
-    width: 200,
-    margin: 10,
-    textAlign: "center"
-  },
-  img: {
-    width: "100%",
-    height: 150,
-    objectFit: "cover"
-  },
-  btn: {
-    display: "inline-block",
-    padding: "10px 15px",
-    marginTop: 10,
-    background: "#0C3866",
-    color: "#fff",
-    borderRadius: 8,
-    textDecoration: "none"
-  }
-};
+export default ProductCart
